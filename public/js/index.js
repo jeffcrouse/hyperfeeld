efbd72f8b6a019f1df8c89bda3e04615f7a60359
Jeff Crouse <jefftimesten@gmail.com>
2013-08-11 15:29:26 -0400

    foo

---
+++ public/js/index.js
@@ -0,0 +1,57 @@
+var socket = null;
+
+function accessoryDidConnect() {
+
+}
+
+function accessoryDidDisconnect() {
+
+}
+
+function webViewDidStartLoad() {
+
+}
+
+function webViewDidFinishLoad() {
+
+}
+
+function dataReceived(data) {
+
+}
+
+function message(msg, style){  
+    $('body').append('<p class="'+style+'">'+msg+"</p>");  
+}  
+
+function connect( host ){  
+	
+    try {
+		socket = new WebSocket(host);  
+
+		message('Socket Status: '+socket.readyState, "event");  
+
+		socket.onopen = function(){  
+			message('Socket Status: '+socket.readyState+' (open)', 'event');  
+		}  
+
+		socket.onmessage = function(msg){  
+			message('Received: '+msg.data, 'message');  
+		}  
+
+		socket.onclose = function(){  
+			message('Socket Status: '+socket.readyState+' (Closed)', 'event');  
+		}             
+
+	} catch(exception){  
+		message('Error'+exception, 'error');  
+	}  
+} 
+
+$(function() {
+	$("h1").html("Stuff!");
+	connect("ws://192.237.167.101:8080");
+});
+
+
+message('Connecting', 'event');
